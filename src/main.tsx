bb59369509d22e31244a337c19658d7379883015
Elad Abutbul <eladabutbul61@gmail.com>
2023-12-14 17:13:40 +0200

    install router,RTK,axios and finish the config

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { App } from './App';
+import { Provider } from 'react-redux';
+import store from './redux-tool-kit/store';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
