5e59ee2ad2bbc53f0b93045fcc151affd0e8f76a
Elad Abutbul <eladabutbul61@gmail.com>
2023-12-14 23:03:34 +0200

    handle search is complete autoComplete is done

--- src/pages/Home/homeComp/search/index.tsx
+++ src/pages/Home/homeComp/search/index.tsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useDebounce from "../../../../hooks/useDebounce";
+import useHandleAutoCompleteSearch from "../../../../hooks/useHandleAutoCompleteSearch";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
 
-export const Search = () => {
-     const [input, setInput] = useState<string>('');
+export const Search = ({ selectedOption }) => {
+  const [input, setInput] = useState<string>('');
+  const { debounceValue } = useDebounce(input, 500);
+  const { autoCompleteList, handleAutoCompleteSearch } = useHandleAutoCompleteSearch();
 
-     return (
-          <div><input type="search" value={input} onChange={e => setInput(e.target.value)} />
-               <div>
-                    //autocomplete text
-               </div>
-          </div>
-     )
-}
+  useEffect(() => {
+    const searchAuto = async () => {
+      if (debounceValue !== '' && input !== '')
+        await handleAutoCompleteSearch(debounceValue);
+
+    };
+    searchAuto();
+  }, [debounceValue, input, handleAutoCompleteSearch]);
+
+
+
+  return (
+    <div>
+      <Autocomplete
+        options={autoCompleteList}
+        getOptionLabel={(option) => option.LocalizedName}
+        isOptionEqualToValue={(option, value) => option.Key === value.Key} // Provide a custom comparison function
+        value={selectedOption}
+        style={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            variant="filled"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+        )}
+      />
+    </div>
+  );
+};
