eed8e648c9276c702cee95186ff03e7ca6ceda07
Elad Abutbul <eladabutbul61@gmail.com>
2023-12-16 14:31:01 +0200

    remove from vaforite

--- src/pages/Favorites/favoritesComp/FavoriteCard/index.tsx
+++ src/pages/Favorites/favoritesComp/FavoriteCard/index.tsx
@@ -1,16 +1,36 @@
-import stlyes from '../../Favorites.module.css'
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import stlyes from '../../Favorites.module.css';
+import { removeFavorite } from '../../../../redux-tool-kit/slices/favorites';
+import { FaTimes } from 'react-icons/fa';
 
 export const FavoriteCard = ({ favorite }) => {
-     console.log(favorite.weather.temperature);
+     const dispatch = useDispatch();
+     const [isHovered, setIsHovered] = useState(false);
+
+     const handleDeleteClick = () => {
+          dispatch(removeFavorite({ id: favorite.id }));
+     };
 
      return (
-          <div className={stlyes.favorite}>
-               <h2>
-                    {favorite.name}
-               </h2>
-               <h2>
-                    {favorite.weather.temperature.tempC}
-               </h2>
+          <div className={stlyes.containerCard}>
+               <div
+                    className={stlyes.favorite}
+
+               >
+                    <h2>{favorite.name}</h2>
+                    <h2>{favorite.weather.temperature.tempC}</h2>
+               </div>
+               <div className={stlyes.delete} onClick={handleDeleteClick}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+               >
+                    <FaTimes
+                         size={20}
+                         color={isHovered ? 'red' : 'black'}
+                         className={stlyes.deleteIcon}
+                    />
+               </div>
           </div>
-     )
-}
+     );
+};
